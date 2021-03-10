eba4a74a93ccd49aa7054c77f79adebb28694089
shining-cloud <62930265+shining-cloud@users.noreply.github.com>
2021-03-10 19:28:18 +0800

    Update Simulation - Radon-Nikodym derivatives and measure change.py

--- Simulation - Radon-Nikodym derivatives and measure change.py
+++ Simulation - Radon-Nikodym derivatives and measure change.py
@@ -32,3 +32,29 @@ plt.legend(['P-measure','Q-measure'])
 plt.vlines(x = 2, ymin = 0, ymax = 0.39,linestyle = '--')
 plt.vlines(x = 0, ymin = 0, ymax = 0.39, linestyle = '--')
 plt.plot()
+
+theta = 2
+# Simulate X~N(0,1)
+X = np.random.normal(0, 1, 100000)
+
+# Generate Y~N(theta, 1)
+Y = X+theta
+Y = np.array(sorted(Y))
+
+# Generate Radon-Nikodym derivative by Y(w)
+Z = np.exp((theta**2 - 2*theta * Y)/2)
+
+#Generate Radon-Nikodym derivative by X(w)
+# Z = np.exp((-(theta**2) - 2*theta * X)/2)
+
+# Change measure: P-measure to Q-measure
+dPy = st.norm.pdf(Y,theta, 1)
+dQy = dPy * Z
+
+# Generate plot of distributions under 2 measures
+plt.plot(Y,dQy, color = 'red')
+sns.kdeplot(Y, color = 'blue')
+plt.legend(['P-measure','Q-measure'])
+plt.vlines(x = 2, ymin = 0, ymax = 0.39,linestyle = '--')
+plt.vlines(x = 0, ymin = 0, ymax = 0.39, linestyle = '--')
+plt.plot()
